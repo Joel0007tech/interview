ecf0607038acc37db4cde5df6638bedad1cdb429
Joel <ajiolajoel@gmail.com>
2023-04-28 11:56:04 +0100

    This is the solution to the test

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+   <div>
+    <div className='bg-[#f6f9fd] w-screen h-screen px-14 pt-8 sm:overflow-hidden sm:pt-2 sm:px-0'>
+    <div className="bg">
+      <h3 className='text-center pt-28 text-2xl relative bottom-2 left-6 sm:text-lg sm:relative sm:left-0 sm:tet-center'>Oops, something went<br/>
+  <span className='relative right-20 sm:relative sm:right-14'>wrong...</span> </h3>
+  <p className='text-center relative right-10 sm:relative sm:right-8'>Error-Code: 403</p>  
+</div>
+    </div>
+    
+   </div>
+  
+  )
+}
+
+export default App
